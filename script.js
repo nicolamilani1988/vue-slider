d5bf7dbf61bb1297f1e8f55b8b988c7df22466af
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-04-07 19:12:17 +0200

    inseriti pallini funzionanti

--- script.js
+++ script.js
@@ -4,36 +4,38 @@ function init(){
 
         data: {
             show : "active",          
-            imageNumber : 1,
+            imageNumber : 0,
             imageFormat: ".jpg",
             imagePath : "./img/paesaggio-",
             bgClass: "",
+            point: "point",
         },
 
         methods: {           
             nextPic: function(){
-                if(this.imageNumber == 4){
-                    this.imageNumber = 1;
+                if(this.imageNumber == 3){
+                    this.imageNumber = 0;
                 } else {
                     this.imageNumber = this.imageNumber+1;
                 }              
             },
             prevPic: function(){
-                if(this.imageNumber == 1){
-                    this.imageNumber = 4;
+                if(this.imageNumber == 0){
+                    this.imageNumber = 3;
                 } else {
                     this.imageNumber = this.imageNumber-1;
                 }
             },
             slideShow: function(){
-
                 this.clock = setInterval(this.nextPic,2000);
-                // setInterval( ()=>this.nextPic(),2000) ;
-                
+                // setInterval( ()=>this.nextPic(),2000) ;            
             },
             slideStop: function(){
                 clearInterval(this.clock);
             },
+            selectImg: function(index){
+                this.imageNumber = index;
+            }
             
         }      
             
