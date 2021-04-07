94a0860f0a7f4071a04325d67fe6dd4a64f10587
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-04-07 19:43:27 +0200

    inserite var per permettere di aggiungere foto

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ function init(){
             imagePath : "./img/paesaggio-",
             bgClass: "",
             point: "point",
+            maxNum : 4 // valore da aumentare se aggiungo foto
         },
 
         methods: {           
             nextPic: function(){
-                if(this.imageNumber == 3){
+                if(this.imageNumber == (this.maxNum-1)){
                     this.imageNumber = 0;
                 } else {
                     this.imageNumber = this.imageNumber+1;
@@ -21,7 +22,7 @@ function init(){
             },
             prevPic: function(){
                 if(this.imageNumber == 0){
-                    this.imageNumber = 3;
+                    this.imageNumber = (this.maxNum-1);
                 } else {
                     this.imageNumber = this.imageNumber-1;
                 }
